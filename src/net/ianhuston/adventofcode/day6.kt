fcb2183db373b7f5f83332b535b8d1c800e00dd6
Ian Huston <ihuston@pivotal.io>
2017-12-11 09:21:26 +0000

    Day 6

---
+++ src/net/ianhuston/adventofcode/day6.kt
@@ -0,0 +1,42 @@
+package net.ianhuston.advent
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputText = File("/Users/ihuston/code/advent/data/day6.txt").readLines()
+    val initialBanks = inputText[0].split("\t").map { it.toInt() }
+    println(initialBanks)
+    println(countCycle(initialBanks))
+}
+
+fun redistribute(initialBanks: List<Int>): List<Int> {
+    val banks = initialBanks.toMutableList()
+    val start = argMax(banks) ?: return initialBanks
+    var blocks = banks[start]
+    banks[start] = 0
+    var index = (start + 1) % banks.size
+    while (blocks > 0) {
+        banks[index]++
+        blocks--
+        index = (index + 1) % banks.size
+    }
+    return banks.toList()
+}
+
+fun countCycle(banks: List<Int>): Int{
+    val allPreviousBanks = mutableListOf(banks)
+    var counter = 1
+    var nextBanks = redistribute(banks)
+    while (nextBanks !in allPreviousBanks) {
+        counter++
+        allPreviousBanks.add(nextBanks)
+        nextBanks = redistribute(nextBanks)
+//        println("$counter: $nextBanks")
+    }
+    val previousIndex = allPreviousBanks.indexOf(nextBanks)
+    return counter - previousIndex
+}
+
+fun argMax(l: List<Int>): Int? {
+    return l.indices.maxBy { l[it] }
+}
---
+++ test/net/ianhuston/adventofcode/Day6KtTest.kt
@@ -0,0 +1,29 @@
+package net.ianhuston.advent
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class Day6KtTest {
+
+    @Test
+    fun testRedistribute() {
+        assertEquals(listOf(2,4,1,2), redistribute(listOf(0,2,7,0)))
+        assertEquals(listOf(3,1,2,3), redistribute(listOf(2,4,1,2)))
+        assertEquals(listOf(0,2,3,4), redistribute(listOf(3,1,2,3)))
+        assertEquals(listOf(1,3,4,1), redistribute(listOf(0,2,3,4)))
+        assertEquals(listOf(2,4,1,2), redistribute(listOf(1,3,4,1)))
+    }
+
+    @Test
+    fun testCountCycle() {
+        assertEquals(4, countCycle(listOf(0,2,7,0)))
+    }
+
+    @Test
+    fun testArgMax() {
+        assertEquals(2, argMax(listOf(1,2,3,1)))
+        assertEquals(3, argMax(listOf(1,2,3,4)))
+        assertEquals(2, argMax(listOf(1,2,3,3)))
+        assertNull(argMax(emptyList<Int>()))
+    }
+}

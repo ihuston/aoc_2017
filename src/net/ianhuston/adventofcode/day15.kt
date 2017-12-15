2312af0b226d8baff474a8f7c40c25928a2e0cff
Ian Huston <ihuston@pivotal.io>
2017-12-15 14:06:11 +0000

    Day 15 part 2

--- src/net/ianhuston/adventofcode/day15.kt
+++ src/net/ianhuston/adventofcode/day15.kt
@@ -1,17 +1,17 @@
 package net.ianhuston.adventofcode
 
-import java.math.BigInteger
-
 fun main(args: Array<String>) {
     println(findPairs(634, 301))
 }
 
 fun findPairs(starterA: Long, starterB: Long): Int {
-    var pair = Pair(starterA, starterB)
     var counter = 0
-    for (i in 1..40_000_000) {
-        pair = nextValues(pair)
-        if (isMatch(pair)) counter++
+    var nextA = starterA
+    var nextB = starterB
+    for (i in 1..5_000_000) {
+        nextA = nextValue(nextA, generator=1)
+        nextB = nextValue(nextB, generator=2)
+        if (isMatch(Pair(nextA, nextB))) counter++
     }
     return counter
 }
@@ -20,8 +20,13 @@ fun isMatch(pair: Pair<Long, Long>): Boolean {
     return pair.first.and(0xFFFFL) == pair.second.and(0xFFFFL)
 }
 
-fun nextValues(pair: Pair<Long, Long>): Pair<Long, Long> {
-    val first = (pair.first * 16807) % 2147483647L
-    val second = (pair.second * 48271) % 2147483647L
-    return Pair(first, second)
+fun nextValue(previous: Long, generator: Int): Long {
+    val factors = listOf(16807, 48271)
+    val thisFactor = factors[generator - 1]
+    var trialValue = previous
+    do {
+        trialValue = (trialValue * thisFactor) % 2147483647L
+    } while (trialValue % (4*generator) != 0L)
+
+    return trialValue
 }
--- test/net/ianhuston/adventofcode/Day15KtTest.kt
+++ test/net/ianhuston/adventofcode/Day15KtTest.kt
@@ -7,16 +7,23 @@ class Day15KtTest {
 
     @Test
     fun testFindPairs() {
-        assertEquals(588, findPairs(starterA=65, starterB=8921))
+        assertEquals(309, findPairs(starterA=65, starterB=8921))
     }
 
     @Test
     fun testNextValues() {
-        assertEquals(Pair(1092455L, 430625591L), nextValues(Pair(65, 8921)))
-        assertEquals(Pair(1181022009L, 1233683848L), nextValues(Pair(1092455L, 430625591L)))
-        assertEquals(Pair(245556042L, 1431495498L), nextValues(Pair(1181022009L, 1233683848L)))
-        assertEquals(Pair(1744312007L, 137874439L), nextValues(Pair(245556042L, 1431495498L)))
-        assertEquals(Pair(1352636452L, 285222916L), nextValues(Pair(1744312007L, 137874439L)))
+        assertEquals(1352636452L, nextValue(65, generator=1))
+        assertEquals(1992081072L, nextValue(1352636452L, generator=1))
+        assertEquals(530830436L, nextValue(1992081072L, generator=1))
+        assertEquals(1980017072L, nextValue(530830436L, generator=1))
+        assertEquals(740335192L, nextValue(1980017072L, generator=1))
+
+        assertEquals(1233683848L, nextValue(8921, generator=2))
+        assertEquals(862516352L, nextValue(1233683848L, generator=2))
+        assertEquals(1159784568L, nextValue(862516352L, generator=2))
+        assertEquals(1616057672L, nextValue(1159784568L, generator=2))
+        assertEquals(412269392L, nextValue(1616057672L, generator=2))
+
     }
 
     @Test
@@ -26,6 +33,7 @@ class Day15KtTest {
         assertTrue(isMatch(Pair(245556042, 1431495498)))
         assertFalse(isMatch(Pair(1744312007, 137874439)))
         assertFalse(isMatch(Pair(1352636452, 285222916)))
+        assertTrue(isMatch(Pair(1023762912L, 896885216L)))
     }
 
 
